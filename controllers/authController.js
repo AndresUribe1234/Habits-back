41740e28a14538a493cc621a1ae2e27dbf21a28d
AndresUribe1234 <anduri1997@gmail.com>
2023-03-02 17:37:41 -0500

    jwt token creation functionality ready for signup and login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,34 @@
 const User = require(`${__dirname}/../models/userModel`);
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const newUser = await User.create({ email, password });
+    const { email, password, passwordConfirm } = req.body;
+
+    // 1) Check if email and pasword exist
+    if (!email || !password || !passwordConfirm) {
+      throw new Error("User did not entered all the required fields!");
+    }
+
+    // 2)Check if both passwords are equal
+    if (password !== passwordConfirm) {
+      throw new Error(
+        "Password and password confirm entered need to be the same!"
+      );
+    }
+
+    // 3)Create user
+    const newUser = await User.create({ email, password, passwordConfirm });
+
+    // 4)Create jwt
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
 
     res.status(200).json({
       status: "Success:User was created!",
-      newUser,
+      token,
+      data: { user: newUser },
     });
   } catch (err) {
     res.status(400).json({ status: "User signup failed!", err: err.message });
@@ -17,20 +38,41 @@ exports.signup = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const userToLogin = await User.find({ email: email });
-
-    if (
-      email === userToLogin[0].email &&
-      password === userToLogin[0].password
-    ) {
-      res.status(200).json({
-        status: "Success:User logged in!",
-        user: userToLogin[0],
-      });
-    } else {
-      throw new Error();
+    // 1) Check if email and pasword exist
+    if (!email || !password) {
+      throw new Error("User did not entered all the required fields!");
     }
+
+    // 2) Check if user exists and password is correct
+    const userToLogin = await User.findOne({ email: email }).select(
+      "+password"
+    );
+
+    const correct = await userToLogin.correctPassword(
+      userToLogin.password,
+      password
+    );
+
+    console.log("correct", correct);
+    console.log(userToLogin);
+
+    if (!userToLogin || !correct) {
+      throw new Error("Incorrect user or password!");
+    }
+
+    // 4)Create jwt
+    const token = jwt.sign({ id: userToLogin._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    // 5)Send okay to client
+    res.status(200).json({
+      status: "Success:User logged in!",
+      token,
+      data: { user: userToLogin },
+    });
   } catch (err) {
+    console.log(err);
     res.status(400).json({ status: "User login failed!", err: err.message });
   }
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,57 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true,
+    lowercase: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Input needs to be an email",
+    },
     trim: true,
     required: [true, "User must enter a password!"],
   },
   name: String,
-  password: { type: String, required: [true, "User must enter a password!"] },
-  date: { type: Date, default: Date.now },
+  password: {
+    type: String,
+    minLength: 8,
+    required: [true, "User must enter a password!"],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    minLength: 8,
+    required: [true, "User must enter a password!"],
+  },
+  userCreationDate: { type: Date, default: Date.now },
   habits: {
     type: [String],
     default: undefined,
     required: [false, "User must enter his/hers habits"],
   },
 });
+
+async function cryptPassword(next) {
+  // Check so this ONLY runs when making a change to the password. This guard clause returns if other field is being modified.
+  if (!this.isModified("password")) return next();
+  //   Hash de password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  //   Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+}
+
+userSchema.pre("save", cryptPassword);
+
+userSchema.methods.correctPassword = async function (
+  dbPassword,
+  enteredPassword
+) {
+  return await bcrypt.compare(enteredPassword, dbPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 
